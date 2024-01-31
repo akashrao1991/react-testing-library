44cac73fd666256a7451233678f5f52cb75bce0c
Akash Rao <akashrao1991@gmail.com>
2024-01-31 16:24:19 +0530

    added transformer to fix the svg asset not found issue

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,18 @@
+// const { compilerOptions } = require('./tsconfig.json');
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: "jsdom",
+  transform:{
+    ".+\\.(css|less|sass|scss|png|jpg|gif|ttf|woff|woff2|svg)$": "jest-transform-stub",
+  }
+  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' } ),
+  // testEnvironment: 'node',
+  // moduleNameMapper: {
+  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+  //     '<rootDir>/__mocks__/fileMock.js',
+  //   '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  // },
+  
 };
